ceb22ad8d738f501a552dd8ccedb1dc2ade20f77
AkshayTidake <akshaytidake0918@gmail.com>
2016-12-22 15:12:06 +0530

    done

--- src/java/LoginClass.java
+++ src/java/LoginClass.java
@@ -2,7 +2,21 @@ import java.sql.*;
 
 public class LoginClass {
     public static boolean validate(String name,String pass){
-        return false;
-    
+         boolean st=false;
+        
+        try{
+            Class.forName("com.mysql.jdbc.Driver");
+            Connection con=DriverManager.getConnection("jdbc:mysql://localhost:3306/clubit","root","akshay123");
+            PreparedStatement ps=con.prepareStatement("select * from login_table where username=? and password=?");
+            ps.setString(1,name);
+            ps.setString(2, pass);
+            ResultSet rs=ps.executeQuery();
+            st=rs.next();
+        }
+        catch(Exception e){
+            e.printStackTrace();
+        }
+        return st;
     }
+    
 }
