a4cb4a1ac8e3c405cf0877712330bf6976addccf
AkshayTidake <akshaytidake0918@gmail.com>
2016-12-22 14:57:50 +0530

    login table updated

--- src/java/DbConnection.java
+++ src/java/DbConnection.java
@@ -1,62 +1,32 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+import java.io.*;
+import java.sql.*;
+import javax.servlet.*;
+import javax.servlet.http.*;
 
-import java.io.IOException;
-import java.io.PrintWriter;
-import javax.servlet.ServletException;
-import javax.servlet.http.HttpServlet;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
 
 public class DbConnection extends HttpServlet {
-
-    /**
-     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
-     * methods.
-     *
-     * @param request servlet request
-     * @param response servlet response
-     * @throws ServletException if a servlet-specific error occurs
-     * @throws IOException if an I/O error occurs
-     */
-    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
-            throws ServletException, IOException {
-        response.setContentType("text/html;charset=UTF-8");
-        try (PrintWriter out = response.getWriter()) {
-            /* TODO output your page here. You may use following sample code. */
-            out.println("<!DOCTYPE html>");
-            out.println("<html>");
-            out.println("<head>");
-            out.println("<title>Servlet DbConnection</title>");            
-            out.println("</head>");
-            out.println("<body>");
-            out.println("<h1>Servlet DbConnection at " + request.getContextPath() + "</h1>");
-            out.println("</body>");
-            out.println("</html>");
-        }
-    }
-
-   
-    @Override
-    protected void doGet(HttpServletRequest request, HttpServletResponse response)
-            throws ServletException, IOException {
-        processRequest(request, response);
-    }
-
         @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
-        processRequest(request, response);
+       
+          response.setContentType("text/html;charset=UTF-8");
+          PrintWriter out = response.getWriter();
+        
+        String Username=request.getParameter("uname");
+        String Password=request.getParameter("pswd");
+       
+         if(LoginClass.validate(Username,Password)){
+            RequestDispatcher r=request.getRequestDispatcher("welcome.jsp");
+            r.forward(request, response);
+        }
+        else{
+            RequestDispatcher r2=request.getRequestDispatcher("index.jsp");
+            r2.include(request, response);
+        } 
         
     }
 
    
-    @Override
-    public String getServletInfo() {
-        return "Short description";
-    }
+  
 
 }
